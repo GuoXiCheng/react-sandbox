5daa68f5f8c2cb980ca97a9e470a83efa27a45bd
GuoXiCheng <1377994267@qq.com>
2023-08-27 22:04:14 +0800

    update graph

--- src/app/graph-visualization.tsx
+++ src/app/graph-visualization.tsx
@@ -25,12 +25,12 @@ const GraphVisualization: React.FC<GraphVisualizationProps> = (props) => {
 
         // append之前清理
         svg.selectAll("*").remove();
-        
+
         const width = window.innerWidth;
         const height = window.innerHeight;
 
         const simulation = d3.forceSimulation(props.nodes)
-            .force("link", d3.forceLink(props.links).id((d: any) => d.id))
+            .force("link", d3.forceLink(props.links).id((d: any) => d.id).distance(100))
             .force("charge", d3.forceManyBody())
             .force("center", d3.forceCenter(width / 2, height / 2));
 
@@ -50,23 +50,45 @@ const GraphVisualization: React.FC<GraphVisualizationProps> = (props) => {
             .attr("r", 10)
             .attr("fill", "#bababa");
 
+        const nodeText = svg.selectAll(null)
+            .data(props.nodes)
+            .enter()
+            .append("text")
+            .text(d => d.label)
+            .style("text-anchor", "middle")
+            .style("font-size", 12)
+            .style("fill", "black");
+
         node.call(d3.drag()
             .on("start", dragstarted as any)
             .on("drag", dragged as any)
             .on("end", dragended as any) as any);
 
+        // 鼠标悬浮事件
+        node.on("mouseover", function () {
+            d3.select(this).style("cursor", "pointer");
+        })
+
+        node.on("mouseout", function () {
+            d3.select(this).style("cursor", "default");
+        })
+
         simulation.on("tick", ticked);
 
         function ticked() {
             link
-                .attr("x1", (d:any) => d.source.x)
-                .attr("y1",  (d:any) => d.source.y)
-                .attr("x2",  (d:any) => d.target.x)
-                .attr("y2",  (d:any) => d.target.y);
+                .attr("x1", (d: any) => d.source.x)
+                .attr("y1", (d: any) => d.source.y)
+                .attr("x2", (d: any) => d.target.x)
+                .attr("y2", (d: any) => d.target.y);
 
             node
-                .attr("cx",  (d:any) => d.x)
-                .attr("cy",  (d:any) => d.y);
+                .attr("cx", (d: any) => d.x)
+                .attr("cy", (d: any) => d.y);
+
+            nodeText
+                .attr("x", d => d.x as number)
+                .attr("y", d => d.y as number + 20)
         }
 
         function dragstarted(event: { active: any; }, d: { fx: any; x: any; fy: any; y: any; }) {
