de21051590ad5eabaa1c1b2ef8cb48ffd095403d
GuoXiCheng <1377994267@qq.com>
2023-08-27 21:41:15 +0800

    update graph

--- src/app/graph-visualization.tsx
+++ src/app/graph-visualization.tsx
@@ -1,37 +1,43 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
+import { SimulationLinkDatum, SimulationNodeDatum } from 'd3';
 
-const GraphVisualization = () => {
+export interface MyNodeDatum extends SimulationNodeDatum {
+    id: string;
+    label: string;
+}
+
+export interface MyLinksDatum extends SimulationLinkDatum<MyNodeDatum> {
+    source: string;
+    target: string;
+}
+
+export interface GraphVisualizationProps {
+    nodes: MyNodeDatum[],
+    links: MyLinksDatum[]
+}
+
+const GraphVisualization: React.FC<GraphVisualizationProps> = (props) => {
     const svgRef = useRef(null);
 
     useEffect(() => {
-        const nodes = [
-            { id: "note1", label: "Note 1" },
-            { id: "note2", label: "Note 2" },
-            { id: "note3", label: "Note 3" },
-            { id: "note4", label: "Note 4" },
-            { id: "note5", label: "Note 5" }
-            // Add more nodes as needed
-        ];
-
-        const links = [
-            { source: "note1", target: "note2" },
-            // Add more links as needed
-        ];
-
         const svg = d3.select(svgRef.current);
-        const width = +svg.attr("width");
-        const height = +svg.attr("height");
 
-        const simulation = d3.forceSimulation(nodes as any)
-            .force("link", d3.forceLink(links).id((d: any) => d.id))
+        // append之前清理
+        svg.selectAll("*").remove();
+        
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        const simulation = d3.forceSimulation(props.nodes)
+            .force("link", d3.forceLink(props.links).id((d: any) => d.id))
             .force("charge", d3.forceManyBody())
             .force("center", d3.forceCenter(width / 2, height / 2));
 
         const link = svg.append("g")
             .attr("class", "links")
             .selectAll("line")
-            .data(links)
+            .data(props.links)
             .enter().append("line")
             .attr("stroke", "#999")
             .attr("stroke-width", 2);
@@ -39,7 +45,7 @@ const GraphVisualization = () => {
         const node = svg.append("g")
             .attr("class", "nodes")
             .selectAll("circle")
-            .data(nodes)
+            .data(props.nodes)
             .enter().append("circle")
             .attr("r", 10)
             .attr("fill", "#bababa");
@@ -79,10 +85,10 @@ const GraphVisualization = () => {
             d.fx = null;
             d.fy = null;
         }
-    }, []);
+    }, [props.links, props.nodes]);
 
     return (
-        <svg ref={svgRef} width={800} height={800}></svg>
+        <svg ref={svgRef} width='100vw' height='100vh'></svg>
     );
 };
 
