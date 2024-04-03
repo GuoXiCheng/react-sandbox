8c2fbeca0bc143c3a98a6fb44714436e5714e295
Xicheng Guo <xicheng_guo@wistron.com>
2024-04-03 14:07:44 +0800

    update hook demo

--- src/pages/styles-demo/ExternalStyleA.tsx
+++ src/pages/styles-demo/ExternalStyleA.tsx
@@ -2,6 +2,6 @@ import './ExternalStyle.css';
 
 export default function ExternalStyleA() {
     return (
-        <div className="external-style-active">External Style A</div>
+        <div className="external-active">External Style A</div>
     );
 }
