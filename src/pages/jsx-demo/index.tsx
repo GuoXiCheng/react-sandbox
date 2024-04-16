e80ddeb81ec1323513d638732c14bcd004701781
Xicheng Guo <xicheng_guo@wistron.com>
2024-04-16 15:00:22 +0800

    add props children

--- src/pages/jsx-demo/index.tsx
+++ src/pages/jsx-demo/index.tsx
@@ -1,4 +1,6 @@
 import BasicJSX from "./BasicJSX";
+import ChildrenBasic from "./ChildrenBasic";
+import ChildrenWithName from "./ChildrenWithName";
 import RenderList from "./RenderList";
 import RenderWithAnd from "./RenderWithAnd";
 import RenderWithIf from "./RenderWithIf";
@@ -7,15 +9,17 @@ import UseExpression from "./UseExpression";
 import UsePic from "./UsePic";
 
 export default function JSXDemo() {
-    return (
-        <>
-            <BasicJSX />
-            <UseExpression />
-            <UsePic />
-            <RenderList />
-            <RenderWithIf isShow={false} />
-            <RenderWithAnd isShow={true} />
-            <RenderWithTernaryExpression isShow={true} />
-        </>
-    );
+  return (
+    <>
+      <BasicJSX />
+      <UseExpression />
+      <UsePic />
+      <RenderList />
+      <RenderWithIf isShow={false} />
+      <RenderWithAnd isShow={true} />
+      <RenderWithTernaryExpression isShow={true} />
+      <ChildrenBasic />
+      <ChildrenWithName />
+    </>
+  );
 }
